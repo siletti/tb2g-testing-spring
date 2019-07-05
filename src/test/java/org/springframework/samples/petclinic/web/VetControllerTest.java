1cf23e5c37e260b69896f78f416f9318c6f1d976
Alberto Siletti <alberto.siletti@gmail.com>
2019-07-05 12:14:34 +0200

    lecture 139

---
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -0,0 +1,60 @@
+package org.springframework.samples.petclinic.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.model.Vets;
+import org.springframework.samples.petclinic.service.ClinicService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+    @Mock
+    Map<String, Object> model;
+    List<Vet> vets = new ArrayList<>();
+    @Mock
+    private ClinicService clinicService;
+    @InjectMocks
+    private VetController vetController;
+
+    @BeforeEach
+    void setUp() {
+        Vet vet = new Vet();
+        vets.add(vet);
+
+        given(clinicService.findVets()).willReturn(vets);
+
+    }
+
+    @Test
+    void showVetList() {
+        //when
+        String view = vetController.showVetList(model);
+        // then
+        then(clinicService).should().findVets();
+        then(model).should().put(anyString(), any());
+        assertThat("vets/vetList".equalsIgnoreCase(view));
+    }
+
+    @Test
+    void showResourcesVetList() {
+        //when
+        Vets vets = vetController.showResourcesVetList();
+        // then
+        then(clinicService).should().findVets();
+        assertThat(vets.getVetList()).hasSize(1);
+    }
+}
