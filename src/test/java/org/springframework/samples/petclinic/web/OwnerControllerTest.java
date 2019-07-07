a376f9ffa98dbfdbf93de9df31b20620c35f8f5a
Alberto Siletti <alberto.siletti@gmail.com>
2019-07-07 15:02:01 +0200

    lecture 164

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,36 +1,90 @@
 package org.springframework.samples.petclinic.web;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.reset;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@ExtendWith(MockitoExtension.class) -- xor:
+@ExtendWith(MockitoExtension.class)
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
-    @InjectMocks
+    //   @InjectMocks
     @Autowired
     OwnerController ownerController;
 
-    @Mock
+    // @Mock
     @Autowired
     ClinicService clinicService;
 
     MockMvc mockMvc;
 
+    Collection<Owner> results;
+
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
 
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(clinicService);
+    }
+
+    @Test
+    void testReturnListOfOwners() throws Exception {
+        given(clinicService.findOwnerByLastName("")).willReturn(Lists.newArrayList(new Owner(), new Owner()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"));
+
+        then(clinicService).should().findOwnerByLastName(stringArgumentCaptor.capture());
+
+        assertThat(stringArgumentCaptor.getValue()).isEqualToIgnoringCase("");
+    }
+
+    @Test
+    void testFindByNameNotFound() throws Exception {
+        mockMvc.perform(get("/owners")
+                .param("lastName", "Dont find ME!"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void testFindByNameOneFound() throws Exception {
+        Owner owner = new Owner();
+        owner.setLastName("pippo");
+        owner.setId(5);
+        given(clinicService.findOwnerByLastName("pippo")).willReturn(Collections.singleton(owner));
+        mockMvc.perform(get("/owners")
+                .param("lastName", "pippo"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/5"));
     }
 
     @Test
